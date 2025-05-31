fd2b6fb89220655794703f4c458bbcab521cd601
vergil6144 <adityadas0807@gmail.com>
2025-05-31 20:47:31 +0530

    node

--- wd14/1.js
+++ wd14/1.js
@@ -1,39 +1,14 @@
-class usr{
-    constructor(un){
-        //proprties
-        this.username =un
+const http = require('http'); //imports http
+const server = http.createServer((req,res)=>{
+    //request and response
+    console.log('-----------------------------------------------------------------')
+    console.log(req.url)
+    if(req.url ==='/'){
+        res.end('sex')
     }
-    login(){
-        console.log('loggedin')
-        return this;
+    if(req.url==='/abt'){
+        res.end('about us')
     }
-    score(){
-        console.log('score')
-    }
-}
-
-class admin extends usr{
-    constructor(un, t){
-        super(un)
-        this.title = t
-    }
-    delete2(usr){
-        users = users.filter((user)=>{
-            return user.username !== usr.username //users.filter 
-        })
-    }
-
-}
-
-const usr1 = new usr('sexyback'); 
-const usr2 = new usr('p'); 
-const ad = new admin('ad','j')
-
-usr1.login().login().score() //because return this returns the object we can chain methods
-
-console.log(usr1.login()) // will log usr object because thats what its returning
-let users = [usr1, usr2, ad]
-console.log(users)
-ad.delete2(usr1)
-console.log(users)
-console.log(ad)
+    res.end(`<h1>sex</h1>`) //this is what it goes to when a non existent address is put
+})
+server.listen(5000)
